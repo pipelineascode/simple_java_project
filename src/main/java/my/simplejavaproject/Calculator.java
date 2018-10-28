4aaddefb4ce76b4a501ec5ddc960367c4d59f76a
pipelineascodecourse <benjichung@yahoo.com>
2018-10-28 14:54:30 +0800

    initial commit

--- src/main/java/my/simplejavaproject/Calculator.java
+++ src/main/java/my/simplejavaproject/Calculator.java
@@ -7,4 +7,19 @@ public class Calculator {
 		return a+b;
 	}
 
+	public int minus(int a, int b)
+	{
+		return a-b;
+	}
+	
+	public int divide(int a, int b)
+	{
+		return a/b;
+	}
+	
+	public int multiply(int a, int b)
+	{
+		return a*b;
+	}
+	
 }
--- src/test/java/my/simplejavaproject/CalculatorTest.java
+++ src/test/java/my/simplejavaproject/CalculatorTest.java
@@ -14,4 +14,23 @@ public class CalculatorTest {
 		assertEquals(3, result);
 		
 	}
+	
+	@Test
+	public void testMinus()
+	{
+	
+		Calculator calculator = new Calculator();
+		int result = calculator.minus(4, 1);
+		assertEquals(3, result);
+		
+	}
+	
+	@Test
+	public void testDivide()
+	{
+	
+		Calculator calculator = new Calculator();
+		int result = calculator.divide(6, 2);
+		assertEquals(3, result);		
+	}	
 }
